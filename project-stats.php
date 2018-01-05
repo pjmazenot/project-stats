acacf9091695daceec050610b0b4f8bbba53e1ab
Pierre-Julien MAZENOT <pj.mazenot@gmail.com>
2018-01-04 22:46:45 -0500

    Add stats property + line processing function with comment and empty lines supports

--- project-stats.php
+++ project-stats.php
@@ -20,6 +20,22 @@ final class ProjectStats {
 	/** @var array Target directory(ies) */
 	private $dir = [];
 
+	/** @var bool Flag used to detect multi-line comments*/
+	private $commentStarted = false;
+
+	/** @var array Project stats */
+	private $stats = [
+		'included-dirs' => 0,
+		'excluded-dirs' => 0, // Excluded by `--exclude-dirs`
+		'included-files' => 0,
+		'excluded-files' => 0, // Excluded by `--exclude-files-ext` and `--exclude-files`
+		'code-lines' => 0,
+		'empty-lines' => 0,
+		'comment-lines' => 0,
+		'chars' => 0,
+		'size' => 0,
+	];
+
 	/**
 	 * ProjectStats constructor.
 	 *
@@ -55,6 +71,65 @@ final class ProjectStats {
 		}
 		if(empty($this->dir)) {
 			$this->log('error', 'You need to include at least one directory');
+		}
+
+	}
+
+	/**
+	 * Process a line
+	 *
+	 * @param string $line Line content
+	 */
+	private function processLine($line) {
+
+		// Increment the character count
+		$this->stats['chars'] += strlen($line);
+
+		// Get a version of the line without spaces, line breaks and tabs
+		$cleanLine = trim($line);
+
+		if(empty($cleanLine)) {
+
+			// Increment the empty line count when the line is empty
+			$this->stats['empty-lines']++;
+
+		} else {
+
+			// Check if the line is a comment and increment the comment line count if it's and inline comment
+			if(
+				substr($line, 0, 1) == '#'
+				|| substr($line, 0, 2) == '//'
+			) {
+				$this->stats['comment-lines']++;
+				return;
+			}
+
+			// Check if the line is a block comment start and increment the comment line count if it's the case
+			if(substr($line, 0, 2) == '/*') {
+
+				$this->stats['comment-lines']++;
+				$this->commentStarted = true;
+				return;
+
+			}
+
+			// Count multi-line comment lines
+			if($this->commentStarted) {
+
+				$this->stats['comment-lines']++;
+
+				// Check if the line is a comment end
+				if(strpos($line, '*/')) {
+					$this->commentStarted = false;
+				}
+
+				return;
+
+			}
+
+			// Increment the code line count
+			$this->stats['code-lines']++;
+
 		}
 
 	}
