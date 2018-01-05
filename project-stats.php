207141101987236d6c260c00e1cc706b55266bc4
Pierre-Julien MAZENOT <pj.mazenot@gmail.com>
2018-01-04 22:29:55 -0500

    Add support for CLI calls and CLI output

--- project-stats.php
+++ project-stats.php
@@ -11,6 +11,9 @@
  */
 final class ProjectStats {
 
+	/** @var string Source of the script call (cli) */
+	private $runFrom;
+
 	/** @var array Options */
 	private $options;
 
@@ -21,9 +24,51 @@ final class ProjectStats {
 	 */
 	public function __construct($options) {
 
+		// Verify and set the script caller
+		if(!empty($options['run-from']) && in_array($options['run-from'], ['cli'])) {
+			$this->runFrom = $options['run-from'];
+		} else {
+			$this->log('error', 'Caller not supported');
+		}
+
 		// Set options
 		$this->options = $options;
 
 	}
+
+	/**
+	 * Log an message
+	 *
+	 * With the CLI if a message has the $level == "error" the process is stopped
+	 *
+	 * @param string $level (error,warning,info)
+	 * @param string $message
+	 */
+	private function log($level, $message) {
+
+		if($this->runFrom == 'cli') {
+
+			if(isset($this->options['h'])) {
+				echo '[' . strtoupper($level) . '] ' . $message . PHP_EOL;
+			}
+
+			if($level == 'error') {
+				die;
+			}
+
+		}
+
+	}
+
+}
+
+// CLI
+if(php_sapi_name() == 'cli') {
+
+	$options = [];
+	$options['run-from'] = 'cli';
+	$projectStats = new ProjectStats($options);
+
+} else {
 
 }
